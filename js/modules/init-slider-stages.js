e16e0be83c011be241bed0eff33739f4af88b03d
Nastya880 <oidmakeevy@gmail.com>
2024-03-05 12:26:18 +0300

    feat: добавляет disable-состояние для кнопки в слайдере

--- js/modules/init-slider-stages.js
+++ js/modules/init-slider-stages.js
@@ -7,39 +7,54 @@ const tabletScreenSize = 1023;
 let currentIndexSlide = 1;
 
 const showSlides = (n) => {    
-  if (n > slidesStages.length) {
-    currentIndexSlide = 1;
-  }
-  if (n < 1) {
-      currentIndexSlide = slidesStages.length;
-  }
-
-  slidesStages.forEach((slide, index) => {
-    slide.classList.remove('is-visible');
-    sliderStagesDot[index].classList.remove('is-active');
-    slide.classList.add('is-hidden');
-  })
-
-  slidesStages[currentIndexSlide - 1].classList.remove('is-hidden');
-  sliderStagesDot[currentIndexSlide - 1].classList.add('is-active');
-  slidesStages[currentIndexSlide - 1].classList.add('is-visible');    
+  if (n >= 1 && n <= slidesStages.length) {
+    slidesStages.forEach((slide, index) => {
+      slide.classList.remove('is-visible');
+      sliderStagesDot[index].classList.remove('is-active');
+      slide.classList.add('is-hidden');
+    })
+  
+    slidesStages[currentIndexSlide - 1].classList.remove('is-hidden');
+    sliderStagesDot[currentIndexSlide - 1].classList.add('is-active');
+    slidesStages[currentIndexSlide - 1].classList.add('is-visible'); 
+  }   
 };
+
+const iniButtonDisabled = (currentIndex) => {
+  if (currentIndex <= 1) {
+    buttonStagesPrev.classList.add('is-disabled');
+    buttonStagesNext.classList.remove('is-disabled');
+  } else if (currentIndex !== slidesStages.length) {
+    buttonStagesPrev.classList.remove('is-disabled');
+    buttonStagesNext.classList.remove('is-disabled');
+  } else if (currentIndex >= slidesStages.length - 1) {
+    buttonStagesPrev.classList.remove('is-disabled');
+    buttonStagesNext.classList.add('is-disabled');
+  } else {
+    buttonStagesPrev.classList.remove('is-disabled');
+    buttonStagesNext.classList.remove('is-disabled');
+  }
+}
 
 const initSliderStages = () => {
   if (slidesStages && buttonStagesNext && buttonStagesPrev && window.innerWidth < tabletScreenSize) {
     showSlides(currentIndexSlide);
+
     buttonStagesNext.addEventListener('click', () => {
       showSlides(currentIndexSlide += 1);
+      iniButtonDisabled(currentIndexSlide);
     });
     
     buttonStagesPrev.addEventListener('click', () => {
       showSlides(currentIndexSlide -= 1);
+      iniButtonDisabled(currentIndexSlide);
     });
 
     sliderStagesDot.forEach((dot, index) => {
       dot.addEventListener('click', () => {
         dot.classList.add('is-active');
         showSlides(currentIndexSlide = index + 1);
+        iniButtonDisabled(currentIndexSlide);
       })
     });
   }
